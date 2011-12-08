2e76b5fa2dde53275334f810df7c868c9bfc9e52
Matthew Oltendorf <moltendorf@gmail.com>
2011-12-08 10:18:59 -0800

    New Plugin class.

---
+++ src/com/moltendorf/bukkit/intellidoors/Plugin.java
@@ -0,0 +1,54 @@
+package com.moltendorf.bukkit.intellidoors;
+
+import org.bukkit.Server;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitScheduler;
+
+/**
+ * IntelliDoors plugin for Bukkit.
+ *
+ * @author moltendorf
+ */
+public class Plugin extends JavaPlugin {
+	// Variable context.
+	protected static Plugin instance = null;
+	protected static String name = null;
+	protected static String version = null;
+	protected static BukkitScheduler scheduler = null;
+
+	@Override
+	public synchronized void onDisable() {
+		// Reset our variables.
+		instance = null;
+		name = null;
+		version = null;
+		scheduler = null;
+
+		// Stopped.
+		System.out.println(name+" version "+version+" was disabled.");
+	}
+
+	@Override
+	public synchronized void onEnable() {
+		if (instance != null) {
+			instance.onDisable();
+		}
+
+		final PluginDescriptionFile description = getDescription();
+		final Server server = getServer();
+		final PluginManager manager = server.getPluginManager();
+
+		// Create our variables.
+		instance = this;
+		name = description.getName();
+		version = description.getVersion();
+		scheduler = server.getScheduler();
+
+		// Register events.
+
+		// Started.
+		System.out.println(name+" version "+version+" was initialized.");
+	}
+}
