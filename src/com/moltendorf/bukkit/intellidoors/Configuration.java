98e6f5e2bb71f43c13abce2af4ef794abbdad573
Matthew Oltendorf <moltendorf@gmail.com>
2011-12-12 00:57:09 -0800

    Added basic structure to Configuration.

--- src/com/moltendorf/bukkit/intellidoors/Configuration.java
+++ src/com/moltendorf/bukkit/intellidoors/Configuration.java
@@ -1,10 +1,16 @@
 package com.moltendorf.bukkit.intellidoors;
 
+import org.bukkit.configuration.file.FileConfiguration;
+
 /**
  * Configuration class.
  *
  * @author moltendorf
  */
 class Configuration {
-	
+	protected Configuration() {
+		// Fetch plugin instance and configuration.
+		final Plugin instance = Plugin.instance;
+		final FileConfiguration configuration = instance.getConfig();
+	}
 }
