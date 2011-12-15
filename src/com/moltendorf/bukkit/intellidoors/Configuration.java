eafbb6e753411c110e088bc0f311b0174ad7371e
Matthew Oltendorf <moltendorf@gmail.com>
2011-12-15 10:49:44 -0800

    More configuration options.

--- src/com/moltendorf/bukkit/intellidoors/Configuration.java
+++ src/com/moltendorf/bukkit/intellidoors/Configuration.java
@@ -19,11 +19,17 @@ class Configuration {
 	// Final data.
 	final protected int ticks = 20; // Number of ticks per second.
 
-	final protected int timer = 250; // Minimum time between flipping doors.
 	final protected int close = 100; // Automated door closing delay (in ticks).
 	final protected int delay = 40; // Door shutting from redstone delay (in ticks).
 
-	final protected HashSet<Integer> ignored = new HashSet<Integer>(Arrays.asList(new Integer[] {0}));
-	final protected HashSet<Integer> interactable = new HashSet<Integer>(Arrays.asList(new Integer[] {}));
-	final protected HashSet<Integer> controllable = new HashSet<Integer>(Arrays.asList(new Integer[] {}));
+	final protected int timer = 250; // Minimum time between flipping doors.
+
+	final protected boolean enabled = true; // Whether or not the plugin is enabled at all.
+	final protected boolean	redstone = true; // Manipulate redstone current response.
+
+	final protected HashSet<Integer> ignored = new HashSet<Integer>(Arrays.asList(new Integer[] {0})); // Blocks to ignore in door orientation checking.
+	final protected HashSet<Integer> interactable = new HashSet<Integer>(Arrays.asList(new Integer[] {})); // Doors that can be opened by interacting.
+	final protected HashSet<Integer> standard = new HashSet<Integer>(Arrays.asList(new Integer[] {})); // Doors that will ignore the redstone flag when alone.
+	final protected HashSet<Integer> controllable = new HashSet<Integer>(Arrays.asList(new Integer[] {})); // Doors that can be controlled by redstone.
+	final protected HashSet<Integer> preserved = new HashSet<Integer>(Arrays.asList(new Integer[] {})); // Doors that have their state preserved (their state can be changed by shift+right-clicking).
 }
