05c33c7493b6f70684e3688489944247e2e219a3
Matthew Oltendorf <moltendorf@gmail.com>
2012-03-05 05:31:02 -0800

    Cleaned out more cruft, like our extra logging messages.

--- src/com/moltendorf/bukkit/intellidoors/Plugin.java
+++ src/com/moltendorf/bukkit/intellidoors/Plugin.java
@@ -16,41 +16,21 @@ public class Plugin extends JavaPlugin {
 	// Variable context.
 	protected static Plugin instance = null;
 
-	// Variable data.
-	protected String name = null;
-	protected String version = null;
-
 	@Override
 	public synchronized void onDisable() {
 		// Clear context.
 		instance = null;
-
-		// Clear data.
-		name = null;
-		version = null;
-
-		// Get logger.
-		final Logger logger = getLogger();
-
-		// Stopped.
-		logger.log(Level.INFO, "{0} version {1} disabled.", new Object[] {name, version});
 	}
 
 	@Override
 	public synchronized void onEnable() {
+		// For the most part, this shouldn't be needed.
 		if (instance != null) {
 			instance.onDisable();
 		}
 
 		// Prepare context.
 		instance = this;
-
-		// Get description.
-		final PluginDescriptionFile description = getDescription();
-
-		// Prepare data.
-		name = description.getName();
-		version = description.getVersion();
 
 		// Get server.
 		final Server server = getServer();
@@ -60,11 +40,5 @@ public class Plugin extends JavaPlugin {
 
 		// Register our event listeners.
 		manager.registerEvents(new Listeners(), this);
-
-		// Get logger.
-		final Logger logger = getLogger();
-
-		// Started.
-		logger.log(Level.INFO, "{0} version {1} enabled.", new Object[] {name, version});
 	}
 }
