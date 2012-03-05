77a3c1a0e64de2d1f7f4333a14a7970064129d1a
Matthew Oltendorf <moltendorf@gmail.com>
2012-03-05 06:39:17 -0800

    More framework for the Listeners class.

--- src/com/moltendorf/bukkit/intellidoors/Listener.java
+++ src/com/moltendorf/bukkit/intellidoors/Listener.java
@@ -1,5 +1,8 @@
 package com.moltendorf.bukkit.intellidoors;
 
+import java.util.HashMap;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -12,26 +15,72 @@ import org.bukkit.event.player.PlayerInteractEvent;
  * @author moltendorf
  */
 final class Listeners implements Listener {
-	@EventHandler(priority = EventPriority.LOWEST)
+	final protected HashMap<Integer, Configuration.Door> doors = Plugin.configuration.doors;
+
+	@EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
 	public void PlayerInteractEventHandler(final PlayerInteractEvent event) {
+		// Do we have a block?
 		if (!event.hasBlock()) {
 			return;
 		}
 
-		// Do stuff.
+		// Get our block and type.
+		final Block block = event.getClickedBlock();
+		final int type = block.getTypeId();
+
+		final Configuration.Door configuration = doors.get(type);
+
+		// Do we have configuration data for this?
+		if (configuration == null) {
+			return;
+		}
+
+		// Is this type of door enabled?
+		if (configuration.enabled) {
+			// Do stuff.
+		}
 	}
 
 	@EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
 	public void PlayerInteractEventMonitor(final PlayerInteractEvent event) {
+		// Do we have a block?
 		if (!event.hasBlock()) {
 			return;
 		}
 
-		// Do stuff.
+		// Get our block and type.
+		final Block block = event.getClickedBlock();
+		final int type = block.getTypeId();
+
+		final Configuration.Door configuration = doors.get(type);
+
+		// Do we have configuration data for this?
+		if (configuration == null) {
+			return;
+		}
+
+		// Is this type of door enabled?
+		if (configuration.enabled) {
+			// Do stuff.
+		}
 	}
 
 	@EventHandler(priority = EventPriority.MONITOR)
 	public void BlockRedstoneEventMonitor(final BlockRedstoneEvent event) {
-		// Do stuff.
+		// Get our block and type.
+		final Block block = event.getBlock();
+		final int type = block.getTypeId();
+
+		final Configuration.Door configuration = doors.get(type);
+
+		// Do we have configuration data for this?
+		if (configuration == null) {
+			return;
+		}
+
+		// Is this type of door enabled?
+		if (configuration.enabled) {
+			// Do stuff.
+		}
 	}
 }
