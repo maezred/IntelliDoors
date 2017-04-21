cf6b38e0438a9aaf94f643c17333e213cc02548a
Matthew Oltendorf <matthew@oltendorf.net>
2017-04-20 23:50:31 -0700

    Fix warning about config version being wrong

--- src/main/kotlin/net/moltendorf/bukkit/intellidoors/settings/SettingsUpgrade.kt
+++ src/main/kotlin/net/moltendorf/bukkit/intellidoors/settings/SettingsUpgrade.kt
@@ -5,21 +5,23 @@ import net.moltendorf.bukkit.intellidoors.log
 private const val SETTINGS_VERSION = 5
 
 fun upgradeSettings(settings: GlobalSettings) {
-  var versions = 0
+  if (settings.version < SETTINGS_VERSION) {
+    var versions = 0
 
-  for (i in settings.version..SETTINGS_VERSION) {
-    upgrade[i]?.invoke(settings) ?: continue
+    for (i in settings.version + 1..SETTINGS_VERSION) {
+      upgrade[i]?.invoke(settings) ?: continue
 
-    ++versions
-  }
+      ++versions
+    }
 
-  settings.version = SETTINGS_VERSION
+    settings.version = SETTINGS_VERSION
 
-  if (versions > 0) {
-    val plural = if (versions > 1) "s" else ""
+    if (versions > 0) {
+      val plural = if (versions > 1) "s" else ""
 
-    log.warning("Your config is $versions version$plural out of date. New values have been inferred.")
-    log.warning("It is recommended to back up your config and delete it so the latest can be generated.")
+      log.warning("Your config is $versions version$plural out of date. New values have been inferred.")
+      log.warning("It is recommended to back up your config and delete it so the latest can be generated.")
+    }
   }
 }
 
